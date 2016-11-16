36bb2844abcd781b5f2e93f6a9cfeb82c1da8015
winfredselwyn <winfredselwyn@gmail.com>
2016-11-16 15:28:18 +0000

    added final component

--- client/app_components/RenderResult.js
+++ client/app_components/RenderResult.js
@@ -1,16 +1,19 @@
 import React from 'react'
+import {List, ListItem} from 'material-ui/List'
 
 function RenderResult ({ result, destinationStation }) {
     const { CompositeMiles, RailFare, RailTime } = result.StationToStationInfos[0]
     return (
         <div className="row center-xs">
-            <div className="col-xs-6">
-                {CompositeMiles}<br/>
-                {RailTime}<br/>
-                {destinationStation}<br/>
-                {RailFare.OffPeakTime}<br/>
-                {RailFare.PeakTime}<br/>
-                {RailFare.SeniorDisabled}<br/>
+            <div className="col-xs-7">
+                <List style={{textAlign: 'left'}}>
+                    <ListItem primaryText="Destination" secondaryText={destinationStation} />
+                    <ListItem primaryText="Distance to Destination" secondaryText={CompositeMiles} />
+                    <ListItem primaryText="Time to Destination" secondaryText={RailTime} />
+                    <ListItem primaryText="Ticket price ( Off Peak )" secondaryText={RailFare.OffPeakTime} />
+                    <ListItem primaryText="Ticket price ( Peak )" secondaryText={RailFare.PeakTime} />
+                    <ListItem primaryText="Ticket price ( Seniors/Disabled )" secondaryText={RailFare.SeniorDisabled} />
+                </List>
             </div>
         </div>
     )
